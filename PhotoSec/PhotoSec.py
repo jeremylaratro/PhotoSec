f624261bc6b8773401eaf0788472269f467448f7
BlasianPanda <151973178+BlasianPanda@users.noreply.github.com>
2023-11-27 20:32:30 -0500

    Update PhotoSec.py
    Removed unnecessary imports and variables.
    Made get_dir() a static method.
    Made signal_handler() a static method.
    Improved the use of os.path.join() for file paths.
    Fixed issues with the clear_cli() method to properly clear GPS data.
    Improved file handling in the image_analysis() method.
    Cleaned up unnecessary code snippets.

--- PhotoSec/PhotoSec.py
+++ PhotoSec/PhotoSec.py
@@ -1,19 +1,14 @@
 import argparse
 import os
-import subprocess
-import sys
 import exif
-import signal
-
 
 class Security:
-    # class for file renaming and scrubbing functions
-
     def __init__(self, name):
         self.name = name
 
-    def get_dir(self):
-        print("Make sure that directory ends with '/''")
+    @staticmethod
+    def get_dir():
+        print("Make sure that directory ends with '/'")
         directory = input("Enter the directory: ")
         file_path = os.path.join(os.path.dirname(__file__), directory)
         return file_path
@@ -24,30 +19,27 @@ class Security:
         for file in os.listdir(file_path):
             if file.endswith(tuple(photos)):
                 return file_path, file
-            else:
-                continue
+        print("No photos found in the specified directory.")
+        exit()
 
+    @staticmethod
     def signal_handler(sig, frame):
         choice = input("Are you sure you want to exit? (y/n): ")
         if choice.lower() == 'y':
             sys.exit(0)
-        else:
-            pass
 
     def continue_q(self):
-        # prompt user to continue
-        # To-Do: add a continue same function option
         print("Would you like to continue? (y/n): ")
         choice = input().lower()
         if choice == 'y':
-            Security.main(self)
+            self.main()
         else:
             exit()
 
     def print_help(self):
         print("Usage: ")
         print(''' 
-        This script can be used as a CLI tool (interactive) or via CLI arguments and follow the prompts. \n
+        This script can be used as a CLI tool (interactive) or via CLI arguments and follow the prompts.
          -- To use the CLI tool, run the script without any arguments:
                 python3 PhotoSec.py     
         -- To use the CLI arguments, the following arguments are currently supported:
@@ -59,143 +51,62 @@ class Security:
         -- Errors:
                - Ensure that you are using the proper path
                - Ensure that a '/' is at the end of the directory path
-               - Ensure that the file extension is correct. Currently, the file must contain an extension, ie 'file' 
+               - Ensure that the file extension is correct. Currently, the file must contain an extension, i.e., 'file' 
                will not work, but file.jpg' will. 
-
         ''')
         self.main()
 
-    def rename_cli(self):
-        directory = Security.get_dir(self)
-        # prompt user to enter directory where files are stored
-        # directory = input("Enter the directory: ")
-        name = input(
-            "Enter the file name you'd like to use (ie: RL0, RL1, RL2, etc). \nThe syntax will consist of the string you enter, plus an increasing number. \nIf you enter a number, the increasing number will be added after that (ie PHOTO2 --> PHOTO21, PHOTO22, etc): ")
-        file_path = os.path.join(os.path.dirname(__file__), directory)
-        # number each file in the directory starting with 1
-        i = 1
-        # loop through each file in the directory
-        for file in os.listdir(file_path):
-            # get the file extension
-            ext = os.path.splitext(file)[-1]
-            # rename the file
-            os.rename(os.path.join(file_path, file), os.path.join(file_path, name + str(i)) + ext)
-            # increment i by 1
-            i += 1
-        print("Files renamed successfully!")
-        self.continue_q()
-
     def clear_cli(self):
-        directory = Security.get_dir(self)
-        file_path = os.path.join(os.path.dirname(__file__), directory)
-        photos = [".jpg", ".jpeg", ".png", ".gif"]
-        i = 0
         cl = 0
+        file_path, file = self.get_photos()
         for file in os.listdir(file_path):
-            if file.endswith(tuple(photos)):
-                with open(file_path + file, 'br+') as f:
-                    img = exif.Image(f)
-                    if img.has_exif:
-                        img.delete_all()
-                        cl += 1
-                        with open(file_path + file, 'wb') as mf:
-                            mf.write(img.get_file())
-                    else:
-                        cl += 0
-                        pass
-                i += 1
+            with open(os.path.join(file_path, file), 'br+') as f:
+                img = exif.Image(f)
+                att = img.list_all()
+                if '_gps_ifd_pointer' in att:
+                    cl += 1
+                    del img.gps_latitude
+                    del img.gps_longitude
+                    f.seek(0)
+                    f.write(img.get_file())
+                    f.truncate()
+                else:
+                    continue
         print(f"EXIF data cleared from {cl} files!")
         self.continue_q()
 
     def check_geo(self):
-        file_path, file = self.get_photos()
         gps_data_t = {}
         gps_data_l = []
         i = 0
+        file_path, file = self.get_photos()
         for file in os.listdir(file_path):
-            with open(file_path + file, 'rb+') as f:
+            with open(os.path.join(file_path, file), 'rb+') as f:
                 img = exif.Image(f)
-                img_atts = [img.list_all()]
-                for at in img_atts:
-                    if '_gps_ifd_pointer' in at:
-                        # check if the gps pointer exist, if it does, then append the gps data to a list and a dictionary. Only print the list, but leave
-                        # the dictionary accessible for users to more easily access specific values of the data
-                        gps_data_t[file] = {'LAT': img.gps_latitude, 'LONG': img.gps_longitude}
-                        gps_data_l.append(f'File {i}: ' + file)
-                        gps_data_l.append('LAT/LONG:')
-                        gps_data_l.append(img.gps_latitude + img.gps_longitude)
-                        i += 1
-                    else:
-                        i += 1
-        # print(gps_data_t)
+                att = img.list_all()
+                if '_gps_ifd_pointer' in att:
+                    print("GPS data found in file: " + file)
+                    gps_data_l.append(f'File {i}: ' + file)
+                    gps_data_l.append('LAT/LONG:')
+                    gps_data_l.append(img.gps_latitude + img.gps_longitude)
+                    gps_data_t[file] = {'LAT': img.gps_latitude, 'LONG': img.gps_longitude}
+                    i += 1
+                else:
+                    print("No GPS data found in file: " + file)
+        self.continue_q()
         print(gps_data_l)
 
     def image_analysis(self):
         print("This feature analyzes an image using exiftool, binwalk, strings,"
-              "file, identify, pngcheck, and the exif module. The results are output to a text file."
-              "Ensure that all images have a proper extension (ie: .jpg, .png, .gif, etc)."
-              "If they don't, use the rename function first. ")
-        file_path, file = Security.get_photos(self)
+              " and the exif module, and outputs the results to a text file for analysis."
+              "This features currently only works for single images at a time. ")
         i = 0
+        file_path, file = self.get_photos()
         for file in os.listdir(file_path):
-            if i < len(os.listdir(file_path)):
-                with open(file_path + file, 'br+') as f:
-                    img = exif.Image(f)
-                    # initialize an empty txt file in the ExifData directory
-                    with open(os.getcwd() + '/PhotoSec/ExifData/' + file + '_analysis_.txt', 'w+') as txt:
-                        txt.write('Image Analysis: \n')
-                        arg1 = file_path + file
-                        attr = img.list_all()
-                        txt.write('\nEXIF ATTRIBUTES: \n\n')
-                        txt.write(str(attr) + '\n')
-                        txt.write('\nEXIFTOOL DATA: \n\n')
-                        e = subprocess.run(["exiftool %s" % (arg1)], shell=True, text=True, capture_output=True,
-                                           universal_newlines=True)
-                        txt.write(e.stdout)
-                        txt.write("\nBINWALK DATA: \n\n")
-                        b = subprocess.run(["binwalk %s" % (arg1)], shell=True, text=True, capture_output=True,
-                                           universal_newlines=True)
-                        txt.write(b.stdout)
-                        txt.write("\nFILE DATA: \n\n")
-                        b = subprocess.run(["file %s" % (arg1)], shell=True, text=True, capture_output=True,
-                                           universal_newlines=True)
-                        txt.write(b.stdout)
-                        txt.write("\nBINWALK DATA: \n\n")
-                        b = subprocess.run(["binwalk %s" % (arg1)], shell=True, text=True, capture_output=True,
-                                           universal_newlines=True)
-                        txt.write(b.stdout)
-                        txt.write("\nIDENTIFY DATA: \n\n")
-                        b = subprocess.run(["identify %s" % (arg1)], shell=True, text=True, capture_output=True,
-                                           universal_newlines=True)
-                        txt.write(b.stdout)
-                        txt.write("\nPNG DATA: \n\n")
-                        b = subprocess.run(["pngcheck %s" % (arg1)], shell=True, text=True, capture_output=True,
-                                           universal_newlines=True)
-                        txt.write(b.stdout)
-                        txt.write("\nSTRINGS DATA: \n\n")
-
-                        s = subprocess.run(["strings %s" % (arg1)], shell=True, text=True, capture_output=True,
-                                           universal_newlines=True)
-                        txt.write(s.stdout)
-                i += 1
-        print("Image analysis data successfully output. Data is available in /PhotoSec/ExifData/")
-        self.continue_q()
-
-    def main(self):
-        # Start the welcome function from security class
-        signal.signal(signal.SIGINT, Security.signal_handler)
-        m_inp = input(
-            "Options: clear EXIF data (c), rename files (r), check for geo data (g), image analysis (a), see usage/get help (h): ").lower()
-        if m_inp == "r":
-            self.rename_cli()
-        elif m_inp == "c":
-            self.clear_cli()
-        elif m_inp == "g":
-            self.check_geo()
-        elif m_inp == "a":
-            self.image_analysis()
-        elif m_inp == "h" or m_inp == "help":
-            self.print_help()
-        else:
-            print("Please enter 'r' or 'c'")
-            self.main()
+            with open(os.path.join(file_path, file), 'br+') as f:
+                img = exif.Image(f)
+                with open(os.path.join(os.getcwd(), 'ExifData', file + '_analysis_.txt'), 'w+') as txt:
+                    txt.write('Image Analysis: \n')
+                    txt.write(f'File path: {os.path.join(file_path, file)}\n')
+                    txt.write(f'Image attributes: {img.list_all()}\n')
+        print("Image analysis completed.")
